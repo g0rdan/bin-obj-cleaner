689c83809cfc626253bf0961ef1cdfdd6c94357b
Denis Gordin <gordin.dan@gmail.com>
2016-05-28 18:04:47 +0300

    add file

---
+++ binobj.cleaner.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+print "Simple python script for cleaning my projects folder from trash"
+print "*" * 20
+
+for root, dirs, files in os.walk("/Users/g0rdan/Projects"):
+	if os.path.isdir(root):
+		if "/bin" in root or "/obj" in root:
+			shutil.rmtree(root)
+			# print root
+		
+    # 
+    # print dirs
+    # print files
